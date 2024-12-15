9272513801398f4eb76f2da5d94e4c348583dee7
Michael-Perillo <michael@perillo.me>
2024-12-15 11:20:32 -0500

    Day 10 Part 2 Python

--- 2024/Day-10/Python/Part2.py
+++ 2024/Day-10/Python/Part2.py
@@ -1,10 +1,36 @@
 from Util import read_input
+from Part1 import index_is_out_of_bounds
+
+def calculate_trailhead_rating(input_data, start):
+    i, j = start
+    sum_ = 0
+    if index_is_out_of_bounds(input_data, i, j):
+        return 0
+    try:
+        current_value = int(input_data[i][j])
+        target_value = current_value + 1
+    except:
+        return 0
+    if current_value == 9:
+            return 1
+    else:
+        for direction in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            if not index_is_out_of_bounds(input_data, i + direction[0], j + direction[1]):
+                if input_data[i + direction[0]][j + direction[1]] == target_value:
+                    sum_ += calculate_trailhead_rating(input_data, (i + direction[0], j + direction[1]))
+        return sum_
+
+def evaluate_input_part_2(input_data, zero_coords):
+    trailhead_scores = []
+    for zero in zero_coords:
+        trailhead_scores.append(calculate_trailhead_rating(input_data, zero))
+    return sum(trailhead_scores)
 
 
 def main():
     # Read the input file
-    input_data, _ = read_input()
-    print((evaluate_input_part_2(input_data)))
+    input_data, zero_coords = read_input()
+    print((evaluate_input_part_2(input_data, zero_coords)))
 
 if __name__ == "__main__":
     main()
--- 2024/Day-10/Python/test.py
+++ 2024/Day-10/Python/test.py
@@ -1,37 +1,53 @@
 import unittest
 from Util import read_input
 from Part1 import evaluate_input
+from Part2 import evaluate_input_part_2
 
 class TestDay9(unittest.TestCase):
     def test_evaluate_input_simple(self):
-        test_input, test_coords = read_input("2024/Day-10/test_input.txt")
+        test_input, test_coords = read_input("2024/Day-10/test_input/test_input.txt")
         test_result = 1
         self.assertEqual(evaluate_input(test_input, test_coords), test_result)
 
     def test_evaluate_input_2(self):
-        test_input, test_coords = read_input("2024/Day-10/test_input_2.txt")
+        test_input, test_coords = read_input("2024/Day-10/test_input/test_input_2.txt")
         test_result = 2
         self.assertEqual(evaluate_input(test_input, test_coords), test_result)
 
     def test_evaluate_input_3(self):
-        test_input, test_coords = read_input("2024/Day-10/test_input_3.txt")
+        test_input, test_coords = read_input("2024/Day-10/test_input/test_input_3.txt")
         test_result = 4
         self.assertEqual(evaluate_input(test_input, test_coords), test_result)
 
     def test_evaluate_input_4(self):
-        test_input, test_coords = read_input("2024/Day-10/test_input_4.txt")
+        test_input, test_coords = read_input("2024/Day-10/test_input/test_input_4.txt")
         test_result = 3
         self.assertEqual(evaluate_input(test_input, test_coords), test_result)
 
     def test_evaluate_input_5(self):
-        test_input, test_coords = read_input("2024/Day-10/test_input_5.txt")
+        test_input, test_coords = read_input("2024/Day-10/test_input/test_input_5.txt")
         test_result = 36
         self.assertEqual(evaluate_input(test_input, test_coords), test_result)
 
-    # def test_evaluate_input_part_2(self):
-    #     test_input = read_input("2024/Day-10/test_input.txt")
-    #     test_result = 2858
-    #     self.assertEqual(evaluate_input_part_2(test_input), test_result)
+    def test_evaluate_input_part_2_1(self):
+        test_input, test_coords = read_input("2024/Day-10/test_input/input_p2_1.txt")
+        test_result = 3
+        self.assertEqual(evaluate_input_part_2(test_input, test_coords), test_result)
+
+    def test_evaluate_input_part_2_2(self):
+        test_input, test_coords = read_input("2024/Day-10/test_input/input_p2_2.txt")
+        test_result = 13
+        self.assertEqual(evaluate_input_part_2(test_input, test_coords), test_result)
+
+    def test_evaluate_input_part_2_3(self):
+        test_input, test_coords = read_input("2024/Day-10/test_input/input_p2_3.txt")
+        test_result = 227
+        self.assertEqual(evaluate_input_part_2(test_input, test_coords), test_result)
+
+    def test_evaluate_input_part_2_4(self):
+        test_input, test_coords = read_input("2024/Day-10/test_input/test_input_5.txt")
+        test_result = 81
+        self.assertEqual(evaluate_input_part_2(test_input, test_coords), test_result)
 
 
 if __name__ == '__main__':
